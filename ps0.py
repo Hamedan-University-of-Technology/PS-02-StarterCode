546e8fa15f02305b084cca8805f5a355353af058
mohamadreza <mohamad.shabro1382@gmail.com>
2023-05-09 17:50:17 +0430

    the code was debugged

--- ps0.py
+++ ps0.py
@@ -1,10 +1,24 @@
-import math
+from math import log , pow
 
-x = int(input("Enter number x: "))
-y = int(input("Enter number y: "))
+def check(x,y):
+    if x.isnumeric() and y.isnumeric():
+        return float(x),float(y)
+    
+    else :
+        raise ValueError("invalid input")
 
-my_pow = math.pow(x,y)
-my_log = math.log(2,x)
+def my_log (x):
+    if x > 0 :
+        return log(x,2)
+    raise ValueError("invalid input")
 
-print(f"X**y = {my_pow}")
-print(f"log(x) = {my_log}")
+def my_pow(x,y):
+    return pow(x,y)
+
+x = input("Enter number x: ")
+y = input("Enter number y: ")
+
+x , y = check(x,y)
+
+print(f"X**y = {my_pow(x,y)}")
+print(f"log(x) = {my_log(x)}")
