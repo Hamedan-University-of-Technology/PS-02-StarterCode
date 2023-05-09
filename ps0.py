caf7a6e0a5605ec419149e860641822cd88d79fa
mohamadreza <mohamad.shabro1382@gmail.com>
2023-05-09 17:13:33 +0430

    completed program

---
+++ ps0.py
@@ -0,0 +1,10 @@
+import math
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+
+my_pow = math.pow(x,y)
+my_log = math.log(2,x)
+
+print(f"X**y = {my_pow}")
+print(f"log(x) = {my_log}")
